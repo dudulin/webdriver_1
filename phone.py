032a2cdb97c6365d15b18bca22076fafbff4ffab
dudulin <https://github.com/dudulin/webpack4>
2022-06-26 13:03:51 +0800

    * adb 代码规范

--- phone.py
+++ phone.py
@@ -1,1 +1,17 @@
-print(1213)
+# Android SDK 部署 ==》 手机开发者模式 ==》 正规数据线连接 ==》 python规范 adb代码
+import time
+import os
+
+
+def execute(cmd):
+    adbstr = 'adb shell {}'.format(cmd)
+    print(adbstr, 'adbstr')
+    os.system(adbstr)
+
+
+def input_tab(x, y):  # 点击
+    str = 'input tap {} {}'.format(x, y)
+    execute(str)
+
+
+input_tab(50, 600)
