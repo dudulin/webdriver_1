a1a1a7fe97b6d3fae3bf82029430f17b62ab7496
v_weidulin <v_weidulin@tencent.com>
2022-08-25 10:15:10 +0800

    * 点击效果

--- computer2.py
+++ computer2.py
@@ -2,8 +2,9 @@ from time import sleep
 
 from pynput.keyboard import Key
 from pynput.keyboard import Controller
-
+import pyautogui
 import datetime
+import random
 
 
 def get_time(hour=-1, minute=-1, second=-1, microsecond=-1):
@@ -30,15 +31,22 @@ def get_time(hour=-1, minute=-1, second=-1, microsecond=-1):
 now = datetime.datetime.now()
 
 action_time = get_time(18, 0)
+action_time2 = get_time(19, 0)
 
 diff_time = action_time - now
 
-sleep(diff_time.total_seconds())
-
-while True:
+# sleep(diff_time.total_seconds())
+flag = True
+while flag:
     shift = Key.shift
     keyboard = Controller()
     keyboard.press(shift)
     keyboard.release(shift)
     print('已经点击了')
-    sleep(120)
+    pyautogui.moveRel(
+        random.randint(
+            50, 1000), random.randint(
+            50, 1000), duration=3)
+    sleep(5)
+    # if datetime.datetime.now() > action_time2:
+    #     flag = False
